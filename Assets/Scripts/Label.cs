1b8376deedc2f17f0e6e672fb010910444449698
nbergstrand <bergstrand.niklas@gmail.com>
2021-09-17 08:37:53 +0100

    Added UI labels

---
+++ Assets/Scripts/Label.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Label : MonoBehaviour
+{
+    [SerializeField]
+    Transform _lineTarget;
+
+    [SerializeField]
+    Transform _lineStart;
+
+    LineRenderer _line;
+
+    void Start()
+    {
+        _line = GetComponent<LineRenderer>();
+
+        _line.startWidth = 0.005f;
+        _line.endWidth = 0.005f;
+        _line.material.color = Color.white;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _line.SetPosition(0, _lineStart.position);
+        _line.SetPosition(1, _lineTarget.position);
+    }
+}
--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,18 +1,68 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class UIManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    Animator _horseAnimator;
+
+    [SerializeField]
+    int _currentState;
+
+    [SerializeField]
+    GameObject _muscleLabel;
+
+    [SerializeField]
+    GameObject _boneLabel;
+
+    public void NextState()
     {
-        
+        if(_currentState < 2)
+        {
+            _currentState++;
+            _horseAnimator.SetInteger("CurrentState", _currentState);
+        }
+
+        EnableLabel(_currentState);
+
     }
 
-    // Update is called once per frame
-    void Update()
+    public void PreviousState()
     {
-        
+        if( _currentState > 0)
+        {
+            _currentState--;
+            _horseAnimator.SetInteger("CurrentState", _currentState);
+        }
+
+        EnableLabel(_currentState);
     }
+
+
+    void EnableLabel(int state)
+    {
+        switch (state)
+        {
+            case 1:
+                _muscleLabel.SetActive(true);
+                _boneLabel.SetActive(false);
+                break;
+            case 2:
+                _muscleLabel.SetActive(false);
+                _boneLabel.SetActive(true);
+                break;
+
+            default:
+                _muscleLabel.SetActive(false);
+                _boneLabel.SetActive(false);
+                break;
+        }
+
+    }
+
+
+
 }
